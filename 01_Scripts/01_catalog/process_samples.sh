af0305a2b0935e6e93c2b63ba3b1753d91933d1a
Juliana Assis <julianaassis17@gmail.com>
2025-04-09 16:19:54 +0200

    cleaning

--- 01_Scripts/01_catalog/process_samples.sh
+++
@@ -1,8 +0,0 @@
-for i in {01..30}; do
-    # Format the sample name as Mxx
-    sample="M$(printf "%02d" $i)"
-    
-    # Process the file
-    cat "filtered_coverage_${sample}.txt" | cut -f1,3,4,6,7,9 > "filtered_coverage_${sample}_columns.txt"
-done
-
